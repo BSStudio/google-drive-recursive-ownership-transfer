fae60d96c5d6ea4ae2c1f40f5adac42dca1539ec
Olivér Kecskeméty <kecskemety.oliver@gmail.com>
2022-06-12 00:39:07 +0200

    Add move command

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
-# Google Drive Recursive Ownership Transfer Tool
+# Google Drive Recursive Mover & Ownership Transfer Tool
 
 Original idea is from [davidstrauss/google-drive-recursive-ownership](https://github.com/davidstrauss/google-drive-recursive-ownership).
 
+These scripts use Google Drive API v3. The `transfer.py` can recursively give the ownership of all files and folders to a given user while the `move.py` can move all files to a new place which is useful for moving files to a shared drive because moving folders is not supported. 
 
 ### Supported Files
 
@@ -12,8 +13,6 @@ Other Google Accounts such as G Suite for Business or Personal Google Accounts c
 NOTE: Ownership can only be transferred to members of the same G Suite or Google domain. Ex. @gmail.com can only transfer to other @gmail.com addresses.
 
 NOTE: The Google Drive API does not allow suppressing notifications for change of ownership.
-
-CURRENT LIMITATION: Due to e-mail sending the script can only process ~2500 files before Google rejects the requests. 
 
 ### Setup
 
@@ -26,6 +25,11 @@ pip install -r requirements.txt
 
 First, replace the sample `client_secrets.json` with your own [client secrets](https://github.com/googleapis/google-api-python-client/blob/master/docs/client-secrets.md). Don't forget to enable Drive API for your project.
 
+
+#### Transferring ownership
+
+CURRENT LIMITATION: Due to e-mail sending the script can only process ~2500 files before Google rejects the requests.
+
 ```commandline
 python transfer.py [-h] -o OWNER [-f FOLDER]
 ```
@@ -35,6 +39,21 @@ options:
   -h, --help            show this help message and exit
   -o OWNER, --owner OWNER
                         E-mail address of the new owner.
+  -f FOLDER, --folder FOLDER
+                        ID of the Google Drive folder. The user's root directory will be used if left empty.
+```
+
+#### Moving files/folders
+
+```commandline
+python move.py [-h] -d DESTINATION [-f FOLDER]
+```
+
+```
+options:
+  -h, --help            show this help message and exit
+  -d DESTINATION, --destination DESTINATION
+                        ID of the destination folder where the files should be moved to.
   -f FOLDER, --folder FOLDER
                         ID of the Google Drive folder. The user's root directory will be used if left empty.
 ```
---
+++ move.py
@@ -0,0 +1,141 @@
+import argparse
+
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+SCOPES = ["https://www.googleapis.com/auth/drive"]
+BATCH = None
+BATCH_SIZE = 0
+MAXIMUM_BATCH_SIZE = 100  # https://developers.google.com/drive/api/guides/performance#overview
+
+
+def get_drive_service():
+    flow = InstalledAppFlow.from_client_secrets_file("client_secrets.json", SCOPES)
+    creds = flow.run_local_server(port=0)
+    service = build("drive", "v3", credentials=creds)
+    return service
+
+
+def callback(request_id, response, exception):
+    if exception:
+        print(exception)
+    else:
+        print("[✓]", end="")
+
+
+def create_batch(service):
+    global BATCH
+    BATCH = service.new_batch_http_request(callback=callback)
+    global BATCH_SIZE
+    BATCH_SIZE = 0
+
+
+def batch_add(service, file_id, destination):
+    if not BATCH:
+        create_batch(service)
+    BATCH.add(service.files().update(
+        fileId=file_id,
+        fields="id, parents",
+        addParents=destination,
+        supportsAllDrives=True
+    ))
+    global BATCH_SIZE
+    BATCH_SIZE += 1
+    if BATCH_SIZE == MAXIMUM_BATCH_SIZE:
+        BATCH.execute()
+        create_batch(service)
+
+
+def get_or_create_destination_folder(service, destination_folder_id, folder_name):
+    try:
+        folder_name = folder_name.replace("'", "\\'")
+        return service.files().list(
+            q=f"'{destination_folder_id}' in parents and name = '{folder_name}'"
+              f" and mimeType = 'application/vnd.google-apps.folder' and not trashed",
+            fields="files(id)", supportsAllDrives=True, includeItemsFromAllDrives=True).execute().get("files")[0].get(
+            "id")
+    except IndexError:
+        return service.files().create(
+            body={
+                "name": folder_name,
+                "mimeType": "application/vnd.google-apps.folder",
+                "parents": [destination_folder_id]
+            },
+            fields="id",
+            supportsAllDrives=True
+        ).execute().get("id")
+
+
+def remove_empty_folders(service, folder_id_list):
+    for folder_id in folder_id_list:
+        items = service.files().list(
+            q=f"'{folder_id}' in parents and not trashed",
+            fields="files(id)").execute().get("files")
+        if not len(items):
+            try:
+                print(f"Deleting folder '{folder_id}'...", end=" ")
+                service.files().delete(fileId=folder_id).execute()
+                print("[✓]")
+            except HttpError as e:
+                print(f"[x] An error occurred: {e}")
+
+
+def process_all_files(service, destination_folder_id, folder_id, folder_name=None, destination_folder_structure=None):
+    if not folder_name:
+        folder_name = service.files().get(fileId=folder_id).execute().get("name")
+    print(f"\nGathering files in folder '{folder_name}'...")
+
+    if not destination_folder_structure:
+        destination_folder_structure = [destination_folder_id]
+
+    processed_folder_list = []
+    next_page_token = None
+    while True:
+        try:
+            items = service.files().list(q=f"'{folder_id}' in parents and not trashed",
+                                         fields="files(id, name, mimeType), nextPageToken",
+                                         pageToken=next_page_token).execute()
+            for item in items["files"]:
+                if item["mimeType"] == "application/vnd.google-apps.folder":
+                    destination_sub_folder_id = get_or_create_destination_folder(service, destination_folder_id,
+                                                                                 item["name"])
+                    destination_folder_structure.append(destination_sub_folder_id)
+                    processed_folder_list.extend(
+                        process_all_files(service, destination_sub_folder_id, item["id"], item["name"],
+                                          destination_folder_structure))
+                    destination_folder_structure.pop()
+                else:
+                    batch_add(service, item["id"], destination_folder_id)
+
+            next_page_token = items.get("nextPageToken")
+            if not next_page_token:
+                break
+
+        except HttpError as e:
+            print(f"An error occurred: {e}")
+            break
+
+    processed_folder_list.append(folder_id)
+    return processed_folder_list
+
+
+def main():
+    msg = "This script moves all files and folders recursively of a given Google Drive folder."
+    parser = argparse.ArgumentParser(description=msg)
+    parser.add_argument("-d", "--destination", help="ID of the destination folder where the files should be moved to.",
+                        required=True)
+    parser.add_argument("-f", "--folder",
+                        help="ID of the Google Drive folder. The user's root directory will be used if left empty.",
+                        default="root")
+    args = parser.parse_args()
+    print(f"Moving all files to '{args.destination}'")
+    service = get_drive_service()
+    processed_folders = process_all_files(service, args.destination, args.folder)
+    if BATCH:
+        BATCH.execute()
+    remove_empty_folders(service, processed_folders)
+
+
+if __name__ == "__main__":
+    main()
